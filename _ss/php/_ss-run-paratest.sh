2162cb4a53d71adbdd83ebc68d94fad24d5a6815
brandoncamenisch <brandoncamenisch@users.noreply.github.com>
2019-08-06 19:50:34 -0700

    ♾️  ITR - master:

--- _ss/php/_ss-run-paratest.sh
+++ _ss/php/_ss-run-paratest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 coverage_filename="/tmp/coverage-${WORDPRESS_THEME}-$(git rev-parse --short HEAD).xml"
-"${COMPOSER_VENDOR_DIR}"/bin/paratest --exclude-group "${PHPUNIT_EXCLUDE_GROUPS}" --coverage-clover="${coverage_filename}" --verbose=1
+"${COMPOSER_VENDOR_DIR}"/bin/paratest --exclude-group "${PHPUNIT_EXCLUDE_GROUPS}" --coverage-clover="${coverage_filename}" --runner=WrapperRunner --verbose=1
 "${COMPOSER_VENDOR_DIR}"/bin/diffFilter --phpunit "${DIFF_FILE}" "${coverage_filename}"
 set +e
