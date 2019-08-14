fd910c29f29482b89788ac8271336d9026dc3f16
brandoncamenisch <brandoncamenisch@users.noreply.github.com>
2019-08-13 18:44:36 -0700

    📦 NEW - master: adds start-wp ss

---
+++ _ss/global/_ss-start-wp.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [ -z "${COMPOSER_AUTH}" ]
+	then echo "You need to setup COMPOSER_AUTH -> https://getcomposer.org/doc/03-cli.md#composer-auth"; exit 1
+fi
+
+# Start docker
+docker-compose up --build --force-recreate --remove-orphans -d
+
