5a2fffa3a32a5f98cb13a23e1cb0766ae2202483
brandoncamenisch <brandoncamenisch@users.noreply.github.com>
2019-08-13 18:32:03 -0700

    ✨ IMP - master: update ci format into generic

--- _ss/global/_ss-ci.sh
+++ _ss/global/_ss-ci.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
-docker network create pmc-dev_default
-docker-compose up -d --remove-orphans --force-recreate --build
-docker-compose run \
--e BITBUCKET_BRANCH=$BITBUCKET_BRANCH \
--e BITBUCKET_CLONE_DIR=$BITBUCKET_CLONE_DIR \
--e BITBUCKET_COMMIT=$BITBUCKET_COMMIT \
--e BITBUCKET_GIT_SSH_ORIGIN=$BITBUCKET_GIT_SSH_ORIGIN \
--e BITBUCKET_REPO_OWNER=$BITBUCKET_REPO_OWNER \
--e BITBUCKET_REPO_SLUG=$BITBUCKET_REPO_SLUG \
--e BITBUCKET_TAG=$BITBUCKET_TAG \
--e CODECOV_AUTH_TOKEN=$CODECOV_AUTH_TOKEN \
--e DOCKER_HOST=$DOCKER_HOST \
--e PMC_CI_ENCODED_KEY=$PMC_CI_ENCODED_KEY \
---rm test-php
+if ! eval "docker-compose run --rm build-php";
+	then echo -e "ERR: EXIT CODE ${?}"; exit $?
+fi
+
+if ! eval " docker-compose up -d --remove-orphans --force-recreate --build wp";
+	then echo -e "ERR: EXIT CODE ${?}"; exit $?
+fi
+
+if ! eval "docker-compose run --rm test-php";
+	then echo -e "ERR: EXIT CODE ${?}"; exit $?
+fi
