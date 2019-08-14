6a1e8394f5ace8625f9c712702ea5147455d30f5
brandoncamenisch <brandoncamenisch@users.noreply.github.com>
2019-08-13 19:06:07 -0700

    💧 DRI - master: defines workingcl

--- _ss/global/_ss-start-wp.sh
+++ _ss/global/_ss-start-wp.sh
@@ -3,6 +3,4 @@ if [ -z "${COMPOSER_AUTH}" ]
 	then echo "You need to setup COMPOSER_AUTH -> https://getcomposer.org/doc/03-cli.md#composer-auth"; exit 1
 fi
 
-# Start docker
 docker-compose up --build --force-recreate --remove-orphans -d
-
