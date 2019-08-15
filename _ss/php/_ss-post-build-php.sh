4e9fa576f6da11e4061fe48947a74ccc1e062fdf
brandoncamenisch <brandoncamenisch@users.noreply.github.com>
2019-08-14 17:10:57 -0700

    📦 NEW - master: paths

--- _ss/php/_ss-post-build-php.sh
+++ _ss/php/_ss-post-build-php.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 case "$1" in
 	wpcom)
-		git -C ${WEB_ROOT}/wp-content/mu-plugins/vip-wpcom-mu-plugins submodule update --init --recursive
-		ln -svf ${WEB_ROOT}/wp-content/mu-plugins/vip-wpcom-mu-plugins/* ${WEB_ROOT}/wp-content/mu-plugins
-		ln -svf ${WEB_ROOT}/wp-content/plugins/wordpress-vip-plugins/* ${WEB_ROOT}/wp-content/plugins
-		ln -svf ${WEB_ROOT}/wp-content/plugins ${WEB_ROOT}/wp-content/themes/vip
+		git -C  ./vendor/pmc/mu-plugins/vip-wpcom-mu-plugins submodule update --init --recursive
+		ln -svf ./vendor/pmc/mu-plugins/vip-wpcom-mu-plugins/* ./vendor/pmc/mu-plugins
+		ln -svf ./vendor/pmc/plugins/wordpress-vip-plugins/* ./vendor/pmc/plugins
+		ln -svf ./vendor/pmc/plugins ./vendor/pmc/themes/vip
 	;;
 	vipgo)
-		ln -svf ${WEB_ROOT}/wp-content/plugins/pmc-vip-go-plugins/* ${WEB_ROOT}/wp-content/plugins
-		ln -svf ${WEB_ROOT}/wp-content/mu-plugins/vip-go-mu-plugins-built/* ${WEB_ROOT}/wp-content/mu-plugins
+		ln -svfr ./vendor/pmc/mu-plugins/vip-go-mu-plugins-built/* ./vendor/pmc/mu-plugins/
+		ln -svfr ./vendor/pmc/plugins/pmc-vip-go-plugins/* ./vendor/pmc/plugins/
 	;;
 esac
