701bd664b4e007363950b2413b49881bbba834c6
brandoncamenisch <brandoncamenisch@users.noreply.github.com>
2019-08-21 15:04:59 -0700

    📦 NEW - master: adds paratest unique test token coverage filename

--- _ss/php/_ss-run-paratest.sh
+++ _ss/php/_ss-run-paratest.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
-coverage_filename="/tmp/coverage-${WORDPRESS_THEME}-$(git rev-parse --short HEAD).xml"
+if [ "${UNIQUE_TEST_TOKEN}" ]
+	then coverage_filename="/tmp/coverage-${WORDPRESS_THEME}-$(git rev-parse --short HEAD)-${UNIQUE_TEST_TOKEN}.xml"
+	else coverage_filename="/tmp/coverage-${WORDPRESS_THEME}-$(git rev-parse --short HEAD).xml"
+fi
 "${COMPOSER_VENDOR_DIR}"/bin/paratest --exclude-group "${PHPUNIT_EXCLUDE_GROUPS}" --coverage-clover="${coverage_filename}" --runner=WrapperRunner --verbose=1 --stop-on-failure --colors
 "${COMPOSER_VENDOR_DIR}"/bin/diffFilter --phpunit "${DIFF_FILE}" "${coverage_filename}"
 set +e
