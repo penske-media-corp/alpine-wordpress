8760a1df4b02ecb16712f001372a4ff5b042d3ca
brandoncamenisch <brandoncamenisch@users.noreply.github.com>
2019-09-06 16:11:09 -0700

    ✨ IMP - master: removes unneded checks

--- _ss/global/_ss-ci.sh
+++ _ss/global/_ss-ci.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-if ! eval "docker-compose run --rm build-php";
-	then echo -e "ERR: EXIT CODE ${?}"; exit $?
-fi
-
-if ! eval " docker-compose up -d --remove-orphans --force-recreate --build wp";
-	then echo -e "ERR: EXIT CODE ${?}"; exit $?
-fi
-
-if ! eval "docker-compose run --rm test-php";
-	then echo -e "ERR: EXIT CODE ${?}"; exit $?
-fi
+set -e
+docker-compose run --rm build-php;
+docker-compose up -d --remove-orphans --force-recreate --build wp
+docker-compose run --rm test-php
