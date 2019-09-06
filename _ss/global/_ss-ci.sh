a8e39da0a95731d86ac140c95d9dd7e08b8bea4a
brandoncamenisch <brandoncamenisch@users.noreply.github.com>
2019-09-06 16:12:47 -0700

    ♾️  ITR - master:

--- _ss/global/_ss-ci.sh
+++ _ss/global/_ss-ci.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-docker-compose run --rm build-php;
+docker-compose run --rm build-php
 docker-compose up -d --remove-orphans --force-recreate --build wp
 docker-compose run --rm test-php
