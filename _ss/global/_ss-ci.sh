91de375ab4272d77fa5cbfb46ac2d55340eb677c
brandoncamenisch <brandoncamenisch@users.noreply.github.com>
2019-09-09 17:48:49 -0700

    ✅ TST - master:

--- _ss/global/_ss-ci.sh
+++ _ss/global/_ss-ci.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker-compose run --rm build-php
-docker-compose up -d --remove-orphans --force-recreate --build wp
+(docker-compose run --rm build-php false) || echo 'build-php failed $?'
+(docker-compose run --rm test-php false) || echo 'test-php failed $?'
 docker-compose run --rm test-php
