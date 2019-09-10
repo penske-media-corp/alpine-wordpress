49cd667f2f093557274be596afe797ea2f18be5d
brandoncamenisch <brandoncamenisch@users.noreply.github.com>
2019-09-09 17:52:28 -0700

    ♾️  ITR - master:

--- _ss/global/_ss-ci.sh
+++ _ss/global/_ss-ci.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-(docker-compose run --rm build-php false) || echo 'build-php failed $?'
-(docker-compose run --rm test-php false) || echo 'test-php failed $?'
+docker-compose run --rm build-php
 docker-compose run --rm test-php
